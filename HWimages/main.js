5c961dc46f2070dcbfc1f0888e7d71516c698508
Hasan <mehmet.ak@stu.fsm.edu.tr>
2022-11-22 02:28:16 +0300

    hw3 son

---
+++ HWimages/main.js
@@ -0,0 +1,86 @@
+var canvas = document.querySelector('canvas');
+var c = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+//create empty array
+var images = [];
+images.length = 10;
+
+//push the images into array
+
+
+async function getData(){
+    for(var i = 1 ; i < images.length ; i++){
+        images[i] = new Image();
+        //images[i].src = 'Walk (' + i.toString() + ').png';
+    
+      await  fetch('http://127.0.0.1:5500/HWimages/'+'Walk (' + i.toString() + ').png').then(res=>res.blob())
+                                                 .then(blob => {
+                                                    let objectURL = URL.createObjectURL(blob);
+                                                    images[i].src = objectURL;
+                                                 });
+    }
+
+}
+
+getData();
+
+
+var i = 1;
+var y = 0;
+var test;
+
+var kontrol=0;
+
+btnPause.addEventListener("click", function(){
+    if(kontrol==0){
+    clearInterval(test);
+        kontrol=1;
+    }
+    else{
+        test = setInterval(function(){
+            i++;
+            y++;
+            if( i >= 10){
+                i = 1;
+            }
+            if(y<10){
+               // console.log(images[i]);
+            }
+            c.drawImage(images[i],300,300,300,300);
+        },150)
+        kontrol=0;
+    }
+});
+
+btnStop.addEventListener("click", function(){
+    clearInterval(test);
+});
+
+btnNext.addEventListener("click", function(){
+    i++;
+    c.drawImage(images[i],300,300,300,300);
+});
+
+btnPrev.addEventListener("click", function(){
+    i--;
+    c.drawImage(images[i],300,300,300,300);
+});
+
+btnStart.addEventListener("click", function(){
+    i=1;
+    test = setInterval(function(){
+        i++;
+        y++;
+        if( i >= 10){
+            i = 1;
+        }
+        if(y<10){
+           // console.log(images[i]);
+        }
+        c.drawImage(images[i],300,300,300,300);
+    },125)
+});
+
