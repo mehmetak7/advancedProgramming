9d84cfb176710c7b3f818c4485d00464a5cf298d
Hasan <mehmet.ak@stu.fsm.edu.tr>
2022-12-22 18:13:42 +0300

    emin

---
+++ termprojectt/sw.js
@@ -0,0 +1,21 @@
+const CACHE ='termprojectt'
+const FILES = ['TermProject.html','minesweeper.js','sw.js','manifest.json','may.png']
+function installCB(e) {
+  e.waitUntil(
+    caches.open(CACHE)
+    .then(cache => cache.addAll(FILES))
+    .catch(console.log)
+  )
+}
+
+self.addEventListener('install',installCB)
+
+function cacheCB(e) { //cache first
+  let req = e.request
+  e.respondWith(
+    caches.match(req)
+    .then(r1 => r1 || fetch(req))
+    .catch(console.log)
+  )
+}
+self.addEventListener('fetch', cacheCB)
